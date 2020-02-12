bff66c2200104d98fe04a040605ffdd1af283cff
Sbu <kanityi@outlook.com>
2020-02-13 00:44:23 +0200

    Adding git ignore file and finishing my assignment

--- assignment1/routes.js
+++ assignment1/routes.js
@@ -1,4 +1,3 @@
-
 const requestHandler = (req,res) => {
     const url = req.url;
     const method = req.method;
@@ -8,6 +7,7 @@ const requestHandler = (req,res) => {
         res.write('<html>');
         res.write('<head><title>Users Greeting</title></head>');
         res.write('<body><h1>Hello user!</h1></body>');
+        res.write('<form action="/create-user" method="POST"><label>username</label><input type="text" name="message"></input><button type="submit">Create user</button></form>');
         res.write('</html>');
         return res.end();
     }
@@ -20,6 +20,27 @@ const requestHandler = (req,res) => {
         res.write('</html>');
         return res.end();
     }
+
+    if(url === '/create-user' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsebody = Buffer.concat(body).toString();
+            const username = parsebody.split('=')[1];
+            console.log(username);
+            res.statusCode = 302;
+            res.setHeader('Location','/');
+            return res.end();
+        });
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</head></title>');
+    res.write('<body>Hello Nodejs</body>');
+    res.write('</html>');
+    res.end();
 
 };
 
--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const requestHandler = (req, res) => {
     if(url === '/'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Enter Message</head></title>');
+        res.write('<head><title>Enter Any Message</head></title>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
